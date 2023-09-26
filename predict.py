a10d2722fb9f2ac43b6e4caf6ac822daf89c7a73
wwlouis00 <wwlouis00@gmail.com>
2023-09-26 16:40:17 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -8,10 +8,10 @@ model = YOLO("your_model.pt")
 model.info()
 
 # predict your image or video
-results = model.predict(source="your_image",project="your_save_dir",name="your_folder_name",save=True)
+results = model.predict(source="your_image",project="result/your_save_dir",name="your_folder_name",save=True)
 print(results)
 
 # Detecting images(.jpg or .png) in an entire folder.
 for image_path in glob.glob(f'your_image/*jpg'):
-      results = model.predict(source=image_path,project="your_save_dir",name="your_folder_name",save=True)
+      results = model.predict(source=image_path,project="result/your_save_dir",name="your_folder_name",save=True)
       print("\n")
