9de15964ece578f4f74da8adbbfa3b55505190f5
wwlouis00 <wwlouis00@gmail.com>
2023-09-26 16:23:17 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,5 +1,17 @@
 from ultralytics import YOLO
+from ultralytics.yolo.v8.detect.predict import DetectionPredictor
+import glob
+from IPython.display import Image, display
 
-model = YOLO("model/yolov8n-seg.pt")
+# Loading your model.
+model = YOLO("your_model.pt")
+model.info()
 
-model.predict("/home/wwlouis/project/yolov8/datasets/images/church.JPG",show=True,conf=0.4,project="/home/wwlouis/project/yolov8/result/segment",save=True)
+# predict your image or video
+results = model.predict(source="your_image",project="your_save_dir",name="your_folder_name",save=True)
+print(results)
+
+# Detecting images(.jpg or .png) in an entire folder.
+for image_path in glob.glob(f'your_image/*jpg'):
+      results = model.predict(source=image_path,project="your_save_dir",name="your_folder_name",save=True)
+      print("\n")
